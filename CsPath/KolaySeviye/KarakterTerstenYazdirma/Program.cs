7acb8f77e78187d0004c0d8b2fce0daaa9befbb7
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-12-07 16:28:51 +0300

    Karakter tersten yazdirma

---
+++ CsPath/KolaySeviye/KarakterTerstenYazdirma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KarakterTerstenYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TerstenYazdir("Hello World !");
+        }
+        static void TerstenYazdir(string message)
+        {
+            for (int i = message.Length-1; i>= 0; i--)
+            {
+                Console.Write(message[i]);
+            }
+        }
+    }
+}
