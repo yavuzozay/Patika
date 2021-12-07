7a7a594094784b3dfeb759549e1110a2e86e61b8
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-12-07 15:37:10 +0300

    Daire Cizme

---
+++ CsPath/KolaySeviye/DaireCizme/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DaireCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen yarıçap giriniz..");
+            int r = Convert.ToInt32(Console.ReadLine());
+           
+            
+            DrawCircle(r,'.');
+        }
+        static void DrawCircle(int yaricap,char sembol)
+        {
+            int cap = yaricap * 2;
+            int x, y;
+            for (int i = 0; i <= cap ; i++)
+
+            {
+                for (int j = 0; j <= cap ; j++)
+                {
+                    x = yaricap-i;
+                    y =  yaricap-j;
+
+                    if (x * x + y * y == yaricap * yaricap)  Console.Write(sembol);
+                    
+                    else Console.Write("  ");
+                   
+                        
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+         
+    }
+    
+}
+    
+    
+
---
+++ CsPath/KolaySeviye/DaireCizme/README.md
@@ -0,0 +1,8 @@
+# Konsolda Daire Çizme
+
+2 nokta arasındaki uzaklık formulü kullanılabilir.
+
+<img src="formul.PNG" width="200" height="200" /> <br> 
+
+Çemberin merkezinden çemberin üzerindeki bir noktanın uzaklığı yarıçap kadar olmalıdır. For döngüsüyle x ve y eksenlerini çap kadar gezilip nokta arasındaki uzaklık kontrol edilir.
+<img src="cikti.PNG" width="200" height="200" /> <br> 
