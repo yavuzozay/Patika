94cdab57ddb138d508e8c16d7a31a8ef2cbf741b
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-12-07 16:22:46 +0300

    Harf cikarma algoritmasi eklendi

---
+++ CsPath/KolaySeviye/IndextekiHarfiCikarma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IndextekiHarfiCikarma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen kelimenizi ve silmek istediğiniz indexi arasına virgül koyarak giriniz");
+            string[] message = Console.ReadLine().Split(',');
+            string word = message[0];
+            int index = Convert.ToInt32(message[1]);
+            HarfCikar(word, index);
+
+        }
+        static void HarfCikar(string mesaj,int index)
+        {
+            string result="";
+            for (int i = 0; i < mesaj.Length; i++)
+            {
+                if(i!=index)
+                {
+                    result += mesaj[i];
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
