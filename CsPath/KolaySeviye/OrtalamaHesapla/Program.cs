a9d0614630e395852565b771b29291c56236c4e1
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-12-07 09:16:35 +0300

    Ortalama hesaplama | Fibonacci serileri için ortalama hesabı

---
+++ CsPath/KolaySeviye/OrtalamaHesapla/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OrtalamaHesapla
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32( Console.ReadLine());
+       
+            Console.WriteLine(fibonacci.Ortalama(n));
+        }
+    }
+}
---
+++ CsPath/KolaySeviye/OrtalamaHesapla/fibonacci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtalamaHesapla
+{
+    
+     class fibonacci
+    {
+       public static int[] fib(int n)
+        {
+            int []results = new int[n + 2]; 
+            int i;
+            results[0] = 0;
+            results[1] = 1;
+
+            for (i = 2; i <= n; i++)
+            {
+                results[i] = results[i - 1] + results[i - 2];
+             
+            }
+          
+
+            return results;
+        }
+        public static int Total(int n)
+        {
+            int result = 0;
+            foreach (var item in fib(n))
+            {
+                result += item;
+            }
+            return result;
+        }
+
+        public static double Ortalama(int n)
+        {
+            return Convert.ToDouble(Total(n)/Convert.ToDouble(n));
+        }
+
+    }
+}
