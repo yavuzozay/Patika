fa80cd57b666b68ef2e8889e1eb0f90365177c95
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-12-07 10:06:52 +0300

    Ucgen Cizme

---
+++ CsPath/KolaySeviye/UcgenCizme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir değer giriniz");
+            int n = Convert.ToInt32(Console.ReadLine());
+            DrawTriangle(n);  
+        }
+        static void DrawTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j =1; j < n*2; j++)
+                {
+                    if (i==1 && j==n )
+                        Console.Write(" ");
+                    else if (n - i + 1 == j  )
+                        Console.Write(" /");
+                    else if(n + i - 1 == j)
+                        Console.Write(@"\");
+
+                    else if (n == i)
+                        Console.Write("_");
+
+                    else
+                        Console.Write(" ");
+                }
+            
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CsPath/KolaySeviye/UcgenCizme/README..md
@@ -0,0 +1,4 @@
+# Üçgen Çizme
+ Kullanıcıdan alınan inputa göre ekrana içi boş üçgen çizer.
+
+ <img src="sonuc.PNG" width="450" height="255" /> <br> 
